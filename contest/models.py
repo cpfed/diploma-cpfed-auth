525e092840a2cbf88f77fe60efb7b18cecf05c44
chifwin <o.s.merezhko@gmail.com>
2025-02-23 15:37:52 +0500

    export email

--- contest/models.py
+++ contest/models.py
@@ -123,7 +123,7 @@ class UserContest(models.Model):
     @property
     def get_full_reg_with_additional_data(self) -> dict:
         res = self.get_full_reg
-        res.update(self.user.get_user_data_by_fields(['handle', 'first_name', 'last_name']))
+        res.update(self.user.get_user_data_by_fields(['handle', 'first_name', 'last_name', 'email']))
         return res
 
 
