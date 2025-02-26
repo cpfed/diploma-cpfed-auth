3dcddb0125dc79342306b00a1a1964cb29c4fc5b
chifwin <o.s.merezhko@gmail.com>
2025-02-26 22:57:45 +0500

    fix date encoding in JSON

--- contest/utils/json_encoder.py
+++ contest/utils/json_encoder.py
@@ -1,9 +1,13 @@
 import json
+import datetime
+
 from phonenumbers import PhoneNumber
 
 class CustomJSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, PhoneNumber):
+            return str(o)
+        if isinstance(o, datetime.date):
             return str(o)
         return super().default(o)
 
