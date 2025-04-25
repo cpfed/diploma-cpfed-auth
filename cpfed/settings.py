13521cb6227c395e274c0f4c24b258f32e85109e
chifwin <o.s.merezhko@gmail.com>
2025-04-26 02:06:35 +0500

    fix CORT headers for API

--- cpfed/settings.py
+++ cpfed/settings.py
@@ -213,7 +213,8 @@ OQYLYK_JWT_SECRET = os.getenv("OQYLYK_JWT_SECRET", '')
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'https://contest.cpfed.kz',
-    'https://app.oqylyq.kz'
+    'https://app.oqylyq.kz',
+    'https://oauth.telegram.org'
 ]
 
 X_FRAME_OPTIONS = 'ALLOW-FROM https://app.oqylyq.kz'
