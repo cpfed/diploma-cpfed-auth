cbf76694f66a74ad0fab11b3788c1a1d35e5428e
Zharaskhan <55748394+ZharaskhanAman@users.noreply.github.com>
2025-05-18 11:50:17 +0500

    Upd gunicorn

--- docker/web_entrypoint.sh
+++ docker/web_entrypoint.sh
@@ -4,4 +4,4 @@ python manage.py migrate
 python manage.py reset_contest_schedules
 
 echo "--> Starting gunicorn"
-gunicorn cpfed.wsgi:application -b 0.0.0.0:8000
+gunicorn cpfed.wsgi:application -b 0.0.0.0:8000 --workers 3 --timeout 60
